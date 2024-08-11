fb4c3804d8d58df80ad76ad72f5a43a21de7439a
Maahyar Azad <mhyrinc@gmail.com>
2024-08-11 20:12:10 +0330

    Add ApplicationDbContext and UnitOfWork

---
+++ src/NadinSoft.Persistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Reflection;
+
+namespace NadinSoft.Persistence.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
