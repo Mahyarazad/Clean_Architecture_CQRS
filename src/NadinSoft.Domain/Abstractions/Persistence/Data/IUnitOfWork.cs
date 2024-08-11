f196ed6aa1d95c2e7b9d1aeb591f40bac59357c9
Maahyar Azad <mhyrinc@gmail.com>
2024-08-11 20:11:24 +0330

    Add Abstractions (UNitOfWork and IProductRepository)

---
+++ src/NadinSoft.Domain/Abstractions/Persistence/Data/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NadinSoft.Domain.Abstractions.Persistence.Data
+{
+    public interface IUnitOfWork
+    {
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/NadinSoft.Domain/Abstractions/Persistence/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using NadinSoft.Domain.Entities.Product;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NadinSoft.Domain.Abstractions.Persistence.Repositories
+{
+    public interface IProductRepository
+    {
+        Task AddAsync(Product value, CancellationToken cancellationToken = default);
+        Task<Product> GetByIdAsync(Guid productId, CancellationToken cancellationToken = default);
+        Task<List<Product>> GetAllProductsAsync(CancellationToken cancellationToken = default);
+        void UpdateAsync(Product value, CancellationToken cancellationToken = default);
+        Task SoftDeleteAsync(Guid productId, CancellationToken cancellationToken = default);
+    }
+}
