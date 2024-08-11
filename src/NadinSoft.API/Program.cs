79e0037475a3f5fc099f2d32257bf690f913126e
Maahyar Azad <mhyrinc@gmail.com>
2024-08-11 20:21:38 +0330

    Add Services to WebApplication - add Database migrate and creation to Program.cs in API

--- src/NadinSoft.API/Program.cs
+++ src/NadinSoft.API/Program.cs
@@ -1,14 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using NadinSoft.Application;
+using NadinSoft.Persistence;
+using NadinSoft.Persistence.Data;
+using NadinSoft.Presentation.Configuration.Extensions.Swagger;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddApplicationDependencies();
+builder.Services.AddControllers();
+builder.Services.AddPersistenceDependencies(builder.Configuration)
+    .AddSwagger();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwaggerDocumentation();
+    using(var scope = app.Services.CreateScope())
+    {
+        using(var context = scope.ServiceProvider.GetService<ApplicationDbContext>()) 
+        {
+            context!.Database.Migrate();
+            context!.Database.EnsureCreated();
+        }
+    }
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
 
 app.UseHttpsRedirection();
+app.UseSwaggerDocumentation();
 
-
+app.MapControllers();
 
 app.Run();
 
