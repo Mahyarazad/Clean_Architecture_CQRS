e72b25884b25b1e42b724b688769ac6492f17743
Maahyar Azad <mhyrinc@gmail.com>
2024-08-11 20:18:10 +0330

    Add Application Dependecy injections to IoC container - add related packages and references to csproj

---
+++ src/NadinSoft.Application/ApplicationDependencies.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using NadinSoft.Application.Features.Products.Commands.CreateProduct;
+using System.Reflection;
+
+namespace NadinSoft.Application
+{
+    public static class ApplicationDependencies
+    {
+        public static IServiceCollection AddApplicationDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<IValidator<CreateProductCommand>,CreateProductCommandValidator>();
+            services.AddMediatR(config => config.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+            return services;
+        }
+    }
+}
