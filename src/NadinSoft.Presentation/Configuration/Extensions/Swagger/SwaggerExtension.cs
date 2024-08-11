df8c29c44996aa35cdf021cf59a5b24fd70d993e
Maahyar Azad <mhyrinc@gmail.com>
2024-08-11 20:19:56 +0330

    Add Presentation Dependecy to IoC container - add related packages ans references to csproj

---
+++ src/NadinSoft.Presentation/Configuration/Extensions/Swagger/SwaggerExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NadinSoft.Presentation.Configuration.Extensions.Swagger
+{
+    public static class SwaggerExtension
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen( options =>
+            {
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo() { Title = "NadinSoft", Version = "v1"});
+            });
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NadinSoft v1"));
+            return app;
+        }
+    }
+}
