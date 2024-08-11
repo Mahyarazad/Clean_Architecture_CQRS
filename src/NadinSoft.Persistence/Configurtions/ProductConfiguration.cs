8ab9ce9d9eac39ad6c133c4da30bb025c9a8e674
Maahyar Azad <mhyrinc@gmail.com>
2024-08-11 20:12:32 +0330

    Add ProductConfiguration

---
+++ src/NadinSoft.Persistence/Configurtions/ProductConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NadinSoft.Domain.Entities.Product;
+
+
+namespace NadinSoft.Persistence.Configurtions
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Name).HasMaxLength(128);
+            builder.Property(p => p.ManufactureEmail).HasMaxLength(128);
+            builder.Property(p => p.ManufacturePhone).HasMaxLength(128);
+
+            builder.HasIndex(p => new { p.ManufactureEmail, p.ProductDate }, "UniqueIndex_MEmail_PDate").IsUnique(true);
+        }
+    }
+}
