663c6487fce6c10f66e792561ab939103910008d
Maahyar Azad <mhyrinc@gmail.com>
2024-08-11 20:13:43 +0330

    Add PersistenceDependencies and required packages to csproj

---
+++ src/NadinSoft.Persistence/PersistenceDependencies.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NadinSoft.Domain.Abstractions.Persistence.Data;
+using NadinSoft.Domain.Abstractions.Persistence.Repositories;
+using NadinSoft.Persistence.Data;
+using NadinSoft.Persistence.Repositories;
+
+namespace NadinSoft.Persistence
+{
+    public static class PersistenceDependencies
+    {
+        public static IServiceCollection AddPersistenceDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("MSSqlServer"));
+            });
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            return services;
+        }
+    }
+}
