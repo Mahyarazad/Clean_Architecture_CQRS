0a09808b74fd2b5328664f61a0dd4c6574736ed5
Maahyar Azad <mhyrinc@gmail.com>
2024-08-11 20:24:07 +0330

    Add ResultErrorParsers

---
+++ src/NadinSoft.Application/Helpers/ResultErrorParser.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation.Results;
+
+namespace NadinSoft.Application.Helpers
+{
+    public static class ResultErrorParser
+    {
+        public static IEnumerable<string> GetErrorsFromValidator(ValidationResult validationResult)
+        {
+            return validationResult.Errors.Select(x => x.ErrorMessage).ToList();
+        }
+    }
+}
---
+++ src/NadinSoft.Presentation/Helpers/ResultErrorParser.cs
@@ -0,0 +1,16 @@
+﻿using FluentResults;
+using NadinSoft.Domain.Entities.Product;
+
+namespace NadinSoft.Presentation.Helpers
+{
+    public static class ResultErrorParser
+    {
+        public static object ParseResultError(Result<Product> result)
+        {
+            return new
+            {
+                Errors = result.Reasons.Select(x => x.Message).ToList()
+            };
+        }
+    }
+}
